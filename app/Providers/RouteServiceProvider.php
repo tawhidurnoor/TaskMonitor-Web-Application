06f9a200deb560bc5143d326033d6147d3408354
Tawhidur Noor <56801563+tawhidurnoor@users.noreply.github.com>
2022-04-20 15:43:33 +0600

    Minor Changes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -22,6 +22,8 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
+    public const COMPANY_HOME = '/company/home';
+    public const COMPANY_CREATE = '/company/create';
 
     /**
      * Define your route model bindings, pattern filters, etc.
