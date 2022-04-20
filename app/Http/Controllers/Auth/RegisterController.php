f856538806066ccd49c46a335197827fb8f2c2dc
Tawhidur Noor <56801563+tawhidurnoor@users.noreply.github.com>
2022-04-20 12:46:11 +0600

    user registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'user_type' => 'Owner',
             'password' => Hash::make($data['password']),
         ]);
     }
