7505f317d68435484503555260973736e7c1f212
Tawhidur Noor <56801563+tawhidurnoor@users.noreply.github.com>
2022-05-17 13:10:58 +0600

    Screenshot model & migration

---
+++ database/migrations/2022_05_17_070918_create_screenshots_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateScreenshotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('screenshots', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('time_tracker_id');
+            $table->bigInteger('staff_id');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('screenshots');
+    }
+}
