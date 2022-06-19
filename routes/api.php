114dea26f540e14cdcc8ba0faa8cfc97490c2b45
Tawhidur Noor <56801563+tawhidurnoor@users.noreply.github.com>
2022-06-19 12:54:29 +0600

    Api route

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,17 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 */
+
+Route::get('/dextop_login', 'TestApiController@dextop_login');
+
+Route::get('/dextop_projects', 'TestApiController@dextop_projects');
+
+Route::get('/dextop_time_tracker', 'TestApiController@dextop_time_tracker');
+
+Route::get('/dextop_screenshot_duration', 'TestApiController@dextop_screenshot_duration');
+
+Route::get('/dextop_time_tracker_stop', 'TestApiController@dextop_time_tracker_stop');
+
+Route::post('/dextop_test_upload', 'TestApiController@dextop_test_upload');
+
+Route::post('/dextop_no_ui_upload', 'TestApiController@dextop_no_ui_upload');
--- routes/web.php
+++ routes/web.php
@@ -24,20 +24,6 @@ Route::get('/test', function(){
     return view('test');
 });
 
-Route::get('/dextop_login', 'TestApiController@dextop_login');
-
-Route::get('/dextop_projects', 'TestApiController@dextop_projects');
-
-Route::get('/dextop_time_tracker', 'TestApiController@dextop_time_tracker');
-
-Route::get('/dextop_screenshot_duration', 'TestApiController@dextop_screenshot_duration');
-
-Route::get('/dextop_time_tracker_stop', 'TestApiController@dextop_time_tracker_stop');
-
-Route::post('/dextop_test_upload', 'TestApiController@dextop_test_upload');
-
-Route::post('/dextop_no_ui_upload', 'TestApiController@dextop_no_ui_upload');
-
 Route::get('/mail', function(){
     $to_name = 'Tawhidur Noor';
     $to_email = 'tawhidbadhan@gmail.com';
