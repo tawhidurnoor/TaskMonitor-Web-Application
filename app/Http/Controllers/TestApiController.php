b15d8b95c65f07bd059614c231173a0c37ce43bb
Tawhidur Noor <56801563+tawhidurnoor@users.noreply.github.com>
2022-06-20 15:07:54 +0600

    User rlogin password for desktop application

--- app/Http/Controllers/TestApiController.php
+++ app/Http/Controllers/TestApiController.php
@@ -3,37 +3,46 @@
 namespace App\Http\Controllers;
 
 use App\Project;
-use App\ProjectStaff;
 use App\Screenshot;
 use App\Setting;
-use App\Staff;
 use App\TimeTracker;
 use App\User;
 use App\Employee;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
-class TestApiController extends Controller
-{
-    public function dextop_login(Request $request)
-    {
+class TestApiController extends Controller {
+
+    public function dextop_login(Request $request) {
         // $user = User::where('email', $request->email)
         //     ->join('employees', 'employees.employee_id', 'users.id')
         //     ->selectRaw('users.*, employees.screenshot_duration')
         //     ->first();
-            
         // if(!isset($user->screenshot_duration)){
         //     $default_screenshot_duration = Setting::where('user_id', Auth::user()->id)->value('screenshot_duration');
         //     $user->screenshot_duration = $default_screenshot_duration;
         // }
-        
+
         $user = User::where('email', $request->email)->get();
-        return json_encode($user);
+        
+
+        if (count($user) > 0) {
+            $request_password = $request->password;
+            $hashed_password = $user[0]->password;
+            
+            if (Hash::check($request->password, $user[0]->password)) {
+                return json_encode($user);
+            } else {
+                return json_encode([]);
+            }
+            
+        } else {
+            return json_encode($user);
+        }
     }
 
-    public function dextop_projects(Request $request)
-    {
+    public function dextop_projects(Request $request) {
         $email = $request->email;
         $user_id = User::where('email', $email)->value('id');
 
@@ -41,12 +50,11 @@ class TestApiController extends Controller
         // foreach($staff as $stf){
         //     $staff_id = $stf->id;
         // }
-
         // $project_ids = ProjectStaff::where('staff_id', $staff_id)->selectRaw('project_id')->toSql();
         $project_ids = DB::table('project_people')
-            ->where('user_id', $user_id)
-            ->selectRaw('project_id')
-            ->get();
+                ->where('user_id', $user_id)
+                ->selectRaw('project_id')
+                ->get();
 
         $project_staff_ids_array = [];
 
@@ -55,22 +63,20 @@ class TestApiController extends Controller
         }
 
         // $projects = Project::whereIn('id', $project_staff_ids_array)->select('title')->get()->pluck('title');
-        
+
         $project_name_with_id = [];
         $projects = Project::whereIn('id', $project_staff_ids_array)->get();
-        foreach($projects as $project){
-            array_push($project_name_with_id, $project->id."_".$project->title);
+        foreach ($projects as $project) {
+            array_push($project_name_with_id, $project->id . "_" . $project->title);
         }
 
         return json_encode($project_name_with_id);
     }
 
-    public function dextop_time_tracker(Request $request)
-    {
+    public function dextop_time_tracker(Request $request) {
         $email = $request->email;
         $project_id = explode("_", $request->project)[0];
         $task_title = $request->task_title;
-
 
         $user_id = User::where('email', $email)->first()->id;
         // $project_id = Project::where('title', $project)->first()->id;
@@ -79,17 +85,16 @@ class TestApiController extends Controller
         $time_tracker->project_id = $project_id;
         $time_tracker->user_id = $user_id;
         $time_tracker->task_title = $task_title;
-        $time_tracker->start  = date('Y-m-d H:i:s');
+        $time_tracker->start = date('Y-m-d H:i:s');
         $time_tracker->save();
 
         // $response = array($time_tracker);
         // return json_encode($response);
-        
+
         return $time_tracker->id;
     }
 
-    public function dextop_screenshot_duration(Request $request)
-    {
+    public function dextop_screenshot_duration(Request $request) {
         //getting screenshot duration
         $project_id = Timetracker::findOrFail($request->time_tracker_id)->project_id;
 
@@ -97,34 +102,32 @@ class TestApiController extends Controller
         $employer_id = $project->user_id;
 
         $employee_id = User::where('email', $request->email)->first()->id;
-        
+
         $employee = Employee::where([
-            'employer_id' => $employer_id,
-            'employee_id' => $employee_id
-        ])->first();
-        
-        if(isset($employee->screenshot_duration)){
+                    'employer_id' => $employer_id,
+                    'employee_id' => $employee_id
+                ])->first();
+
+        if (isset($employee->screenshot_duration)) {
             $screenshot_duration = $employee->screenshot_duration;
-        }else{
+        } else {
             $screenshot_duration = Setting::where('user_id', $employer_id)->first()->screenshot_duration;
         }
-        
+
         return $screenshot_duration;
     }
 
-    public function dextop_time_tracker_stop(Request $request)
-    {
+    public function dextop_time_tracker_stop(Request $request) {
         $timeTrackerId = $request->timeTrackerId;
 
         $timeTracker = TimeTracker::findOrFail($timeTrackerId);
-        $timeTracker->end  = date('Y-m-d H:i:s');
+        $timeTracker->end = date('Y-m-d H:i:s');
         $timeTracker->save();
 
         return $timeTracker;
     }
 
-    public function dextop_test_upload(Request $request)
-    {
+    public function dextop_test_upload(Request $request) {
         $email = $request->email;
         $timeTrackerId = $request->timeTrackerId;
 
@@ -154,12 +157,11 @@ class TestApiController extends Controller
         $screenshot->save();
     }
 
-    public function dextop_no_ui_upload(Request $request)
-    {
+    public function dextop_no_ui_upload(Request $request) {
         $mac_address = $request->macAddress;
 
         $employee = Employee::where('mac_address', $mac_address)->first();
-        
+
         //creting screenshot object
         $screenshot = new Screenshot();
         $screenshot->user_id = $employee->employee_id;
@@ -169,7 +171,7 @@ class TestApiController extends Controller
             $extention = $file->getClientOriginalExtension();
 
             //naming file
-            $filename = time() . '_' . $mac_address .'.' .$extention;
+            $filename = time() . '_' . $mac_address . '.' . $extention;
             $file->move('captured/', $filename);
 
             $screenshot->image = $filename;
@@ -177,4 +179,5 @@ class TestApiController extends Controller
 
         $screenshot->save();
     }
+
 }
