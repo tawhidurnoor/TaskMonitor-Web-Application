8eb53df2ac8874fa6c4c8c00c35d2ed33de5e2da
Tawhidur Noor <56801563+tawhidurnoor@users.noreply.github.com>
2022-07-02 10:25:57 +0600

    Update index.blade.php

--- resources/views/backend/pricing/index.blade.php
+++ resources/views/backend/pricing/index.blade.php
@@ -256,7 +256,11 @@
                                         </div>
                                         <!--end::Features-->
                                         <!--begin::Select-->
-                                        <a href="#" class="btn btn-sm btn-primary">Select</a>
+                                        <button class="btn btn-sm btn-primary" id="sslczPayBtn"
+                                        token="if you have any token validation"
+                                        postdata="your javascript arrays or objects which requires in backend"
+                                        order="If you already have the transaction generated for current order"
+                                        endpoint="{{ url('/pay-via-ajax') }}">Select</button>
                                         <!--end::Select-->
                                     </div>
                                     <!--end::Option-->
@@ -629,4 +633,30 @@
         <!--end::Post-->
     </div>
     <!--end::Content-->
+@endsection
+
+
+@section('scripts')
+<!-- If you want to use the popup integration, -->
+<script>
+    var obj = {};
+    obj.cus_name = $('#customer_name').val();
+    obj.cus_phone = $('#mobile').val();
+    obj.cus_email = $('#email').val();
+    obj.cus_addr1 = $('#address').val();
+    obj.amount = $('#total_amount').val();
+
+    $('#sslczPayBtn').prop('postdata', obj);
+
+    (function (window, document) {
+        var loader = function () {
+            var script = document.createElement("script"), tag = document.getElementsByTagName("script")[0];
+            // script.src = "https://seamless-epay.sslcommerz.com/embed.min.js?" + Math.random().toString(36).substring(7); // USE THIS FOR LIVE
+            script.src = "https://sandbox.sslcommerz.com/embed.min.js?" + Math.random().toString(36).substring(7); // USE THIS FOR SANDBOX
+            tag.parentNode.insertBefore(script, tag);
+        };
+
+        window.addEventListener ? window.addEventListener("load", loader, false) : window.attachEvent("onload", loader);
+    })(window, document);
+</script>
 @endsection
