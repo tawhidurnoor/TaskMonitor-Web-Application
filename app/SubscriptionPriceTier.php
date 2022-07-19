255c027f30afac757d533263cc860fb4b96ef3c2
seaum23 <seaum333@gmail.com>
2022-07-19 10:41:03 +0600

    added subscription models Added necessary models for subscription module

---
+++ app/SubscriptionPriceTier.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SubscriptionPriceTier extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2022_07_18_154249_create_subscriptions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->string("subscription_name", 100);
+            $table->tinyInteger("screenshot_preserve_duration")->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+}
